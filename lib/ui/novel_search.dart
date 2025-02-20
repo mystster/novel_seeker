c3bec321dfa0ecd193bf25563e25480f9d4d2a7a
mystster <mystster@gmail.com>
2025-02-20 23:50:08 +0900

    feat(search): enhance search dialog with form validation
    このコミットでは、検索ダイアログにフォームバリデーションを追加しました。ユーザーが検索条件を入力するためのフィールドを設け、必須項目の検証を行うことで、より使いやすいインターフェースを提供します。また、依存関係に新しいパッケージを追加し、DartおよびFlutterのバージョンを更新しました。

--- lib/ui/novel_search.dart
+++ lib/ui/novel_search.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
+
+import '../model/novel_search_model.dart';
+
+final _logger = Logger();
 
 class NovelSearch extends HookConsumerWidget {
   const NovelSearch({super.key});
@@ -9,12 +16,13 @@ class NovelSearch extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isSearchDialogOpen = useState(true);
     final searchResult = useState([]);
+    final formKey = useMemoized(GlobalKey<FormBuilderState>.new);
     useEffect(() {
       WidgetsBinding.instance.addPostFrameCallback(
         (timeStamp) {
           if (isSearchDialogOpen.value) {
             showDialog(
-                    builder: (context) => searchDialog(context, ref),
+                    builder: (context) => _searchDialog(context, ref, formKey),
                     context: context)
                 .whenComplete(
               () => isSearchDialogOpen.value = false,
@@ -50,17 +58,63 @@ class NovelSearch extends HookConsumerWidget {
         ));
   }
 
-  Widget searchDialog(BuildContext context, WidgetRef ref) {
+  Widget _searchDialog(BuildContext context, WidgetRef ref,
+      GlobalKey<FormBuilderState> formKey) {
     return AlertDialog(
       title: const Text('検索条件'),
-      content: Container(),
+      content: FormBuilder(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FormBuilderTextField(
+              name: NovelSearchParam.word.name,
+              decoration:
+                  InputDecoration(labelText: NovelSearchParam.word.displayName),
+              validator: FormBuilderValidators.required(
+                  errorText: '検索条件を入れてください', checkNullOrEmpty: true),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              // onChanged: (value) => (formKey.currentState?.validate()),
+            ),
+            FormBuilderTextField(
+              name: NovelSearchParam.notword.name,
+              decoration: InputDecoration(
+                  labelText: NovelSearchParam.notword.displayName),
+            ),
+            FormBuilderCheckboxGroup(
+              name: NovelSearchParam.searchRange.name,
+              decoration: InputDecoration(
+                  labelText: NovelSearchParam.searchRange.displayName),
+              options: [
+                FormBuilderFieldOption(
+                  value: NovelSearchParam.title.name,
+                  child: Text(NovelSearchParam.title.displayName),
+                ),
+                FormBuilderFieldOption(
+                  value: NovelSearchParam.ex.name,
+                  child: Text(NovelSearchParam.ex.displayName),
+                ),
+                FormBuilderFieldOption(
+                  value: NovelSearchParam.keyword.name,
+                  child: Text(NovelSearchParam.keyword.displayName),
+                ),
+                FormBuilderFieldOption(
+                  value: NovelSearchParam.wname.name,
+                  child: Text(NovelSearchParam.wname.displayName),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
       actions: [
         ElevatedButton(
-          child: const Text('検索'),
-          onPressed: () {
-            //TODO: 検索を実行する
-          },
-        ),
+            child: const Text('検索'),
+            onPressed: () {
+              if (formKey.currentState?.saveAndValidate() ?? false) {
+                _logger.d(formKey.currentState?.value);
+              }
+            }),
         ElevatedButton(
           child: const Text('閉じる'),
           onPressed: () {
