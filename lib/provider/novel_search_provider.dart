d2f7c10306ebf39a2947c4dfc72270f1eea6caed
mystster <mystster@gmail.com>
2025-02-22 00:50:05 +0900

    feat(novel-search): implement novel search functionality
    このコミットでは、小説検索機能を追加しました。新しいプロバイダーを作成し、APIから小説情報を取得するためのロジックを実装しました。また、検索結果の表示方法を改善し、結果がない場合のメッセージを追加しました。

---
+++ lib/provider/novel_search_provider.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../model/narou_novel_info.dart';
+
+final novelSearchProvider = FutureProvider.autoDispose
+    .family<List<NarouNovelInfo>, Map<String, dynamic>?>(
+  (ref, arg) async {
+    if (arg == null) {
+      return [];
+    }
+    final baseUrl = Uri.parse('https://api.syosetu.com/novelapi/api/?out=json&lim=500');
+
+    Map<String, dynamic> queryParameters = _flattenListToMap(arg);
+    queryParameters.addAll(baseUrl.queryParameters);
+
+    final accessUrl = baseUrl.replace(queryParameters: queryParameters);
+    _logger.d('access url is ${accessUrl.toString()}');
+    final response = await http.get(accessUrl);
+    final json = jsonDecode(response.body) as List<dynamic>;
+    return NarouNovelInfoCollection.fromJson(json).data ?? [];
+  },
+);
+
+final _logger = Logger();
+
+Map<String, dynamic> _flattenListToMap(Map<String, dynamic> data) {
+  Map<String, dynamic> result = {};
+  data.forEach((key, value) {
+    if (value is List<String>) {
+      for (var element in value) {
+        (result.addAll({element: '1'}));
+      }
+    }
+    if (result.containsKey(key)) {
+      _logger.w('同じキーが存在しています($key)');
+    }
+    if (value != null && value is! List<String>) {
+      result[key] = value;
+    }
+  });
+  return result;
+}
+
+enum NovelSearchParam {
+  word('検索文字'),
+  notword('検索除外文字'),
+  searchRange('検索範囲'),
+  title('タイトル'),
+  ex('あらすじ'),
+  keyword('キーワード'),
+  wname('作者名');
+
+  final String displayName;
+  const NovelSearchParam(this.displayName);
+  @override
+  String toString() => displayName;
+}
--- lib/ui/novel_search.dart
+++ lib/ui/novel_search.dart
@@ -5,7 +5,8 @@ import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
 
-import '../model/novel_search_model.dart';
+import '../model/narou_novel_info.dart';
+import '../provider/novel_search_provider.dart';
 
 final _logger = Logger();
 
@@ -15,16 +16,88 @@ class NovelSearch extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isSearchDialogOpen = useState(true);
-    final searchResult = useState([]);
+    final searchParam = useState<Map<String, dynamic>?>(null);
+    final searchResult = ref.watch(novelSearchProvider(searchParam.value));
     final formKey = useMemoized(GlobalKey<FormBuilderState>.new);
     useEffect(() {
       WidgetsBinding.instance.addPostFrameCallback(
-        (timeStamp) {
+        (_) {
           if (isSearchDialogOpen.value) {
+            // 検索用ダイアログを表示
             showDialog(
-                    builder: (context) => _searchDialog(context, ref, formKey),
-                    context: context)
-                .whenComplete(
+                context: context,
+                builder: (context) => AlertDialog(
+                      title: const Text('検索条件'),
+                      content: FormBuilder(
+                        key: formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            FormBuilderTextField(
+                              name: NovelSearchParam.word.name,
+                              decoration: InputDecoration(
+                                  labelText: NovelSearchParam.word.displayName),
+                              validator: FormBuilderValidators.required(
+                                  errorText: '検索条件を入れてください',
+                                  checkNullOrEmpty: true),
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              // onChanged: (value) => (formKey.currentState?.validate()),
+                            ),
+                            FormBuilderTextField(
+                              name: NovelSearchParam.notword.name,
+                              decoration: InputDecoration(
+                                  labelText:
+                                      NovelSearchParam.notword.displayName),
+                            ),
+                            FormBuilderCheckboxGroup(
+                              name: NovelSearchParam.searchRange.name,
+                              decoration: InputDecoration(
+                                  labelText:
+                                      NovelSearchParam.searchRange.displayName),
+                              options: [
+                                FormBuilderFieldOption(
+                                  value: NovelSearchParam.title.name,
+                                  child:
+                                      Text(NovelSearchParam.title.displayName),
+                                ),
+                                FormBuilderFieldOption(
+                                  value: NovelSearchParam.ex.name,
+                                  child: Text(NovelSearchParam.ex.displayName),
+                                ),
+                                FormBuilderFieldOption(
+                                  value: NovelSearchParam.keyword.name,
+                                  child: Text(
+                                      NovelSearchParam.keyword.displayName),
+                                ),
+                                FormBuilderFieldOption(
+                                  value: NovelSearchParam.wname.name,
+                                  child:
+                                      Text(NovelSearchParam.wname.displayName),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        ElevatedButton(
+                            child: const Text('検索'),
+                            onPressed: ()  {
+                              if (formKey.currentState?.saveAndValidate() ??
+                                  false) {
+                                searchParam.value = formKey.currentState?.value;
+                                Navigator.of(context).pop();
+                              }
+                            }),
+                        ElevatedButton(
+                          child: const Text('閉じる'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        )
+                      ],
+                    )).whenComplete(
               () => isSearchDialogOpen.value = false,
             );
           }
@@ -42,86 +115,28 @@ class NovelSearch extends HookConsumerWidget {
             )
           ],
         ),
-        body: Column(
-          children: [
-            Visibility(
-              visible: searchResult.value.isEmpty,
-              child: const Center(
-                child: Text('No search result'),
+        body: Center(
+          child: switch (searchResult) {
+            AsyncData(:final value) => Column(
+                children: [
+                  Visibility(
+                    visible: value.isEmpty,
+                    child: const Center(
+                      child: Text('No search result'),
+                    ),
+                  ),
+                  Visibility(
+                    visible: value.isNotEmpty,
+                    child: Text(value.length.toString()),
+                  )
+                ],
               ),
-            ),
-            Visibility(
-              visible: searchResult.value.isNotEmpty,
-              child: Container(),
-            )
-          ],
-        ));
-  }
-
-  Widget _searchDialog(BuildContext context, WidgetRef ref,
-      GlobalKey<FormBuilderState> formKey) {
-    return AlertDialog(
-      title: const Text('検索条件'),
-      content: FormBuilder(
-        key: formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FormBuilderTextField(
-              name: NovelSearchParam.word.name,
-              decoration:
-                  InputDecoration(labelText: NovelSearchParam.word.displayName),
-              validator: FormBuilderValidators.required(
-                  errorText: '検索条件を入れてください', checkNullOrEmpty: true),
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              // onChanged: (value) => (formKey.currentState?.validate()),
-            ),
-            FormBuilderTextField(
-              name: NovelSearchParam.notword.name,
-              decoration: InputDecoration(
-                  labelText: NovelSearchParam.notword.displayName),
-            ),
-            FormBuilderCheckboxGroup(
-              name: NovelSearchParam.searchRange.name,
-              decoration: InputDecoration(
-                  labelText: NovelSearchParam.searchRange.displayName),
-              options: [
-                FormBuilderFieldOption(
-                  value: NovelSearchParam.title.name,
-                  child: Text(NovelSearchParam.title.displayName),
-                ),
-                FormBuilderFieldOption(
-                  value: NovelSearchParam.ex.name,
-                  child: Text(NovelSearchParam.ex.displayName),
-                ),
-                FormBuilderFieldOption(
-                  value: NovelSearchParam.keyword.name,
-                  child: Text(NovelSearchParam.keyword.displayName),
-                ),
-                FormBuilderFieldOption(
-                  value: NovelSearchParam.wname.name,
-                  child: Text(NovelSearchParam.wname.displayName),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-      actions: [
-        ElevatedButton(
-            child: const Text('検索'),
-            onPressed: () {
-              if (formKey.currentState?.saveAndValidate() ?? false) {
-                _logger.d(formKey.currentState?.value);
-              }
-            }),
-        ElevatedButton(
-          child: const Text('閉じる'),
-          onPressed: () {
-            Navigator.of(context).pop();
+            AsyncLoading() => const CircularProgressIndicator(),
+            AsyncError(:final error, :final stackTrace) =>
+              Text('Error: $error, ST: $stackTrace'),
+            // TODO: Handle this case.
+            AsyncValue<List<NarouNovelInfo>>() => throw UnimplementedError(),
           },
-        )
-      ],
-    );
+        ));
   }
 }
