72ecef47b242905e808648eeb5db766fb1b4c8be
mystster <mystster@gmail.com>
2025-03-08 21:01:05 +0900

    feat(ui): add additional widget to NovelInfoCard
    NovelInfoCardにadditionalWidgetを追加し、必要に応じて表示されるようにしました。また、NovelSearchで登録状態に応じたアイコンを表示する機能を実装しました。

--- lib/ui/novel_info_card.dart
+++ lib/ui/novel_info_card.dart
@@ -7,12 +7,14 @@ import 'util_ui.dart';
 class NovelInfoCard extends StatelessWidget {
   final NarouNovelInfo info;
   final PopupMenuButton<dynamic>? popupMenuButton;
+  final Widget? additionalWidget;
   final double padding;
 
   const NovelInfoCard({
     super.key,
     required this.info,
     this.popupMenuButton,
+    this.additionalWidget,
     this.padding = 4.0,
   });
 
@@ -22,36 +24,41 @@ class NovelInfoCard extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.all(padding),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   LayoutBuilder(builder: (context, constraints) {
                     final span = TextSpan(
                         text: info.title,
                         style: Theme.of(context).textTheme.titleMedium);
                     final tp = TextPainter(
-                        text: span, textDirection: TextDirection.ltr, maxLines: 2);
+                        text: span,
+                        textDirection: TextDirection.ltr,
+                        maxLines: 2);
                     tp.layout(maxWidth: constraints.maxWidth);
                     final numLines = tp.computeLineMetrics().length;
-              
+
                     return Text(
-                                        info.title,
-                                        style: Theme.of(context).textTheme.titleMedium,
-                                        maxLines: numLines, // 行数を動的に決定
-                                        overflow: TextOverflow.ellipsis,
-                                      );
+                      info.title,
+                      style: Theme.of(context).textTheme.titleMedium,
+                      maxLines: numLines, // 行数を動的に決定
+                      overflow: TextOverflow.ellipsis,
+                    );
                   }),
                   const SizedBox(height: 4),
                   textWithIcon(Icons.person, info.writer),
                   const SizedBox(height: 4),
                   Row(
                     children: [
-                      textWithIcon(Icons.book, '${info.generalAllNo.toString()}話'),
+                      textWithIcon(
+                          Icons.book, '${info.generalAllNo.toString()}話'),
                       Expanded(
-                        child: textWithIcon(Icons.category, info.genre.toString()),
+                        child:
+                            textWithIcon(Icons.category, info.genre.toString()),
                       ),
                     ],
                   ),
@@ -59,7 +66,8 @@ class NovelInfoCard extends StatelessWidget {
                   Row(
                     children: [
                       tag(context, info.isStop, '停止中'),
-                      tag(context, info.novelType == NovelType.shortStory, '短編'),
+                      tag(context, info.novelType == NovelType.shortStory,
+                          '短編'),
                       tag(context, info.isR15, 'R15'),
                       tag(context, info.isTensei, '転生'),
                       tag(context, info.isBl, 'BL'),
@@ -68,8 +76,17 @@ class NovelInfoCard extends StatelessWidget {
                 ],
               ),
             ),
-            if (popupMenuButton != null)
-                          SizedBox(width: 30, child: popupMenuButton!)
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (additionalWidget != null) additionalWidget!,
+                if (additionalWidget != null && popupMenuButton != null) 
+                  const SizedBox(height: 40), 
+                if (popupMenuButton != null) 
+                  popupMenuButton!
+              ],
+            )
           ],
         ),
       ),
